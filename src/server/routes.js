55eeaffaa962fc90cc550cd17d77a7d5e33b01ea
Shawn Inder <shawninder@gmail.com>
2014-04-17 22:31:15 -0400

    simple 404

--- src/server/routes.js
+++ src/server/routes.js
@@ -2,4 +2,7 @@ exports.setup = function (app) {
 	app.get('/', function (req, res) {
 		res.sendfile('index.html');
 	});
+	app.use('*', function (req, res) {
+		res.status(404).send('404');
+	});
 };
